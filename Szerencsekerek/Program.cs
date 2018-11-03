a160a69acb9a29b152d0173883b4beaec62a64f1
David Ebert <ebertek@gmail.com>
2018-11-03 22:31:57 +0100

    Add basic Wheel

---
+++ Szerencsekerek/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Szerencsekerek
+{
+    class Wheel
+    {
+        private readonly int[] layout = new int[] { 0, 1700, 5500, 1100, 6000, 1100, 2000, 1100, 1500, 5500, 1300, 4000, 900, 1100, 11000, 1600, 1200, 4000, 1500, 1200, 6000, 1000, 13000 };
+        public int Spin()
+        {
+            Random rnd = new Random();
+            int idx = rnd.Next(layout.Length);
+            return layout[idx];
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wheel Wheel = new Wheel();
+            int Spun = Wheel.Spin();
+            Console.WriteLine(Spun);
+            Console.ReadKey();
+        }
+    }
+}
